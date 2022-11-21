df4b91b61212be6a3e31d30ef26ec7e2310f9e85
EC2 Default User <ec2-user@ip-172-31-6-235.ap-northeast-1.compute.internal>
2022-11-21 14:34:45 +0000

    kadai2 doing OK

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,8 +20,21 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
-
+  
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
   private
   def book_params
     params.require(:book).permit(:title,:body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
   root to:'homes#top'
-  post 'books'=> 'books#create'
-  get 'books/index'=> 'books#index'
-  get 'books/:id'=> 'books#show',as:'book'
-  get 'books/edit'
+  resources:books
   
 end
